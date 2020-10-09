aaa0f1e5f8c353a39d5fead200e563402bc1af93
Abby Paul <birddog1198@gmail.com>
2020-10-09 15:34:58 -0500

    Tutorials

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+
+	void Start()
+	{
+		GenerateColor();
+	}
+
+	public void GenerateColor()
+	{
+		GetComponent<Renderer>().sharedMaterial.color = Random.ColorHSV();
+		//LogColor();
+	}
+
+	public void ResetColor()
+	{
+		GetComponent<Renderer>().sharedMaterial.color = Color.white;
+		//LogColor();
+		Debug.Log("Color Reset");
+	}
+
+	public void LogColor()
+	{
+		Debug.Log("Cube Color: " + GetComponent<Renderer>().sharedMaterial.color);
+	}
+}
